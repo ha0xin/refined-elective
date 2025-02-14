482a5045b68f29b24b675e8c227f37ea1e2a1222
ha0xin <139300141+ha0xin@users.noreply.github.com>
2025-02-14 09:37:56 +0800

    Bump version to 0.4.3 for course conflict highlighting script

--- helper.user.js
+++ helper.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        Elective网站课程冲突高亮
 // @namespace   https://greasyfork.org/users/1429968
-// @version     0.4.2
+// @version     0.4.3
 // @description 分析已选课程与所有课程的时间冲突，并用颜色标记
 // @author      ha0xin
 // @match       https://elective.pku.edu.cn/elective2008/edu/pku/stu/elective/controller/courseQuery/*
